01154903fb3d59116eb3d0a25172f244d6e30a99
Levine <n01362563@unf.edu>
2021-04-07 19:56:32 -0400

    Added Search function for users

--- src/Example1.java
+++ src/Example1.java
@@ -7,6 +7,7 @@
 
 // You need to import the java.sql package to use JDBC
 import java.sql.*;
+import java.util.Scanner;
 //import java.util.Properties;
 import java.io.*;  
 
@@ -21,38 +22,25 @@ class Example1
 	//Properties prop = new Properties();
 	//InputStream input = null;
 	  
-    System.out.print("userid: ");
-    String uid = getString();
+    //System.out.print("userid: ");
+	Scanner reader = new Scanner(System.in); 
+	  
+    String uid = "n01362563";
 
-    System.out.print("password: ");
-    String pword = getString();
+    //System.out.print("password: ");
+    String pword = "Spring20212563";
     String url = "jdbc:mysql://cisvm-winsrv-mysql1.unfcsd.unf.edu:3306/group3";
     
-//	try {
-//
-//		input = new FileInputStream("config.properties");
-//
-//		// load a properties file
-//		prop.load(input);
-//
-//		// get the property value and print it out
-//		 url = prop.getProperty("database");
-//		 //uid = prop.getProperty("dbuser");
-//		 //pword = prop.getProperty("dbpassword");
-//
-//
-//	} catch (IOException ex) {
-//		ex.printStackTrace();
-//	} finally {
-//		if (input != null) {
-//			try {
-//				input.close();
-//			} catch (IOException e) {
-//				e.printStackTrace();
-//			}
-//		}
-//	}
+    System.out.println("\n User Options ");
+    System.out.println("----------------------------");
+    System.out.println("1. Search");
+    System.out.println("2. Checkout (rent) / buy");
+    System.out.println("3. Return");
+    System.out.println("4. Check Balance");
+    System.out.println("5. Pay Balance\n");
     
+    System.out.print("> "); int choice = reader.nextInt();
+    System.out.println("");
 
     
     // Load the Oracle JDBC driver
@@ -63,19 +51,48 @@ class Example1
     Class.forName("com.mysql.cj.jdbc.Driver");
     Connection conn=DriverManager.getConnection(  url, uid, pword);
     
+    if (choice == 1)
+    {
+  
+    	System.out.print("> ");
+    	String title = reader.next();
+    	title += reader.nextLine();
+    	Statement stmt = conn.createStatement();
+    	String q1 = "select * from group3.isbn where isbn.Title='" + title+"'";
+    	ResultSet rset = stmt.executeQuery(q1);
+    	
+    	System.out.println("");
+    	System.out.println(q1);
+    	System.out.println("");
+    	
+    	while (rset.next ()) {
+          String isbn = rset.getString("ISBN");
+          String author = rset.getString("AuthorName");
+          String category = rset.getString("Category");
+          String publisher = rset.getString("Publisher");
+          String format = rset.getString("Format");
+          System.out.println("ISBN: " + isbn + "\n" 
+        		  				+ "Author: " + author + "\n" 
+        		  				+ "Category: " + category + "\n"
+        		  				+ "Publisher: " + publisher + "\n"
+        		  				+ "Format: " + format);
+
+      } // while rset
+    	
+    }
 
     // Create a Statement
-    Statement stmt = conn.createStatement ();
+//    Statement stmt = conn.createStatement ();
+//    
+//    String q = "select * from group3.isbn";
+//    
+//    ResultSet rset = stmt.executeQuery(q);
     
-    String q = "select * from group3.isbn";
-    
-    ResultSet rset = stmt.executeQuery(q);
-    
-    while (rset.next ()) {
-        String isbn = rset.getString("ISBN");
-               
-        System.out.println(isbn);
-    } // while rset
+//    while (rset.next ()) {
+//        String isbn = rset.getString("ISBN");
+//               
+//        System.out.println(isbn);
+//    } // while rset
     
   } // main 
 
